65d1381a2aee17c1a124e6da53ddadc0485825b4
mohanr2593 <mohanr2593@gmail.com>
2022-07-02 17:00:39 +0530

    Updated
    Html css js working in good state.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function btnworkexperience() {
+  const x = document.querySelector('.workexperiencesection');
+  x.classList.toggle("hidden");
+}
+
+
+function btneducationqualification() {
+    const x = document.querySelector('.educationdetailssection');
+    x.classList.toggle("hidden");
+  }
+
+
+  function btnprojectdetails(){
+    const x = document.querySelector('.projectdetailssection');
+    x.classList.toggle("hidden");
+  }
